dda868559f8aea621d36e9e58054449cf9505578
GusRodrigues86 <omykronbr@gmail.com>
2024-12-03 23:42:26 -0500

    Definitions for our service constants

---
+++ apod-api/src/constants.hpp
@@ -0,0 +1,10 @@
+#ifndef api_constants_hpp
+#define api_constants_hpp
+namespace APOD::API {
+	class Definitions {
+	public:
+		static constexpr const char* API_SERVICE_NAME = "api-service";
+		static constexpr const char* API_SERVICE_VH = "api-service.virtualhost";
+	};
+}
+#endif // !api_constants_hpp
